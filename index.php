79146d891ece86417469452fb6dca16b6af6ff8b
radoczsanyi <radocz.sandor1231@example.com>
2025-06-12 20:12:49 +0200

    Front Controller tervezés

--- index.php
+++ index.php
@@ -2,52 +2,76 @@
 session_start();
 require_once 'services/db.php';
 require_once 'services/auth.php';
-$error = '';
+
+// eleresi utak
+//
+define('BASE_PATH', dirname(__DIR__));
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'] ?? '';
-    $password = $_POST['password'] ?? '';
+  if (isset($_POST['logout'])) {
+    session_destroy();
+    header("Location: index.php?page=home");
+    exit();
+  }
 
-    $stmt = $db->prepare(query: "SELECT * FROM user WHERE email = ?");
-    $stmt->execute(params: [$email]);
-    $user = $stmt->fetch(mode: PDO::FETCH_ASSOC);
-
-    if ($user && password_verify(password: $password, hash: $user['Password'])) {
-        $_SESSION['user_id'] = $user['ID'];
-        $_SESSION['email'] = $user['Email'];
-        header(header: "Location: main.php");
-        exit;
-    } else {
-        $error = "Hibás email vagy jelszó.";
-    }
+  // navigalas
+  //
+  if (isset($_POST['knives'])) {
+    header("Location: index.php?page=knives");
+    exit();
+  }
+  if (isset($_POST['messages'])) {
+    header("Location: index.php?page=messages");
+    exit();
+  }
+  if (isset($_POST['contact'])) {
+    header("Location: index.php?page=contact");
+    exit();
+  }
+  if (isset($_POST['login'])) {
+    header("Location: index.php?page=login");
+    exit();
+  }
+  if (isset($_POST['main'])) {
+    header("Location: index.php?page=home");
+    exit();
+  }
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bejelentkezés - vaszilijedc.hu</title>
-    <link rel="stylesheet" href="./css/login.css">
-</head>
-<body>
-<div class="page">
-  <div class="container">
-    <div class="left">
-      <div class="login">Bejelentkezés</div>
-      <div class="eula"><a href="register.php">Ha még nincs saját felhasználód itt tudsz regisztrálni.</a></div>
-    </div>
-    <div class="right">
-        <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
-        <form action="index.php" method="POST" class="form">
-            <label for="email">Email cím:</label>
-            <input type="email" name="email" id="email" required>
-            <label for="password">Jelszó:</label>
-            <input type="password" name="password" id="password" required>
-            <input type="submit" id="submit"></input>
-        </form>
-    </div>
-  </div>
-</div>
-</body>
-</html>
+
+// kerjuk le a "page" query stringet, ha nincs akkor automatikusan fooldal
+//
+$page = $_GET['page'] ?? 'home';
+
+switch ($page) {
+  case 'home':
+    include 'pages/home.php';
+    break;
+
+  case 'knives':
+    include 'pages/knives.php';
+    break;
+
+  case 'contact':
+    include 'pages/contact.php';
+    break;
+
+  case 'login':
+    include 'pages/login.php';
+    break;
+
+  case 'register':
+    include 'pages/register.php';
+    break;
+
+  case 'resellers':
+    include 'pages/resellers.php';
+    break;
+
+  case 'messages':
+    include 'pages/messages.php';
+    break;
+
+  default:
+    include 'pages/home.php';
+    break;
+}
---
+++ pages/home.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+  <title>Kezdőlap - vaszilijedc.hu</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+  <link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body>
+  <?php include './components/nav.php'; ?>
+  <div class="jumbotron text-center" style="background-image: url('./images/front.jpg')">
+    <h1>Vaszilijedc.hu</h1>
+    <P>Kések, pengék és multi toolok egy helyen</P>
+    <h1>Bemutatkozás</h1>
+  </div>
+  <br>
+  <div class="container-fluid">
+    <div class="row">
+
+      <div class="col-sm-2"></div> <!-- bal szél -->
+
+      <div class="col-sm-8 middle-block">
+        <div class="row">
+
+          <div class="col-sm-3">
+            <img src="./images/IMG_20180614_163340_1.jpg.webp" alt="Balogh József" class="img-responsive"
+              style="max-width: 100%;">
+          </div>
+
+          <div class="col-sm-9">
+            <h1>Naplónak indult, Bemutató bloggá vált.</h1>
+            <h5>Aztán átalakult valami mássá. Ablakká, amelyben kitekintek a világra, a világ meg betekinthet a
+              gondolataimba: késekről, every day carry felszerelésekről, és az ezek mögött meghúzódó filozófiáról.</h5>
+            <h5>Aztán ennél is több lett. Egy közösség, amelyben együtt, hasonló értékek mentén dolgozunk azért, hogy
+              egy minőségi, kissé talán régimódi találkahely legyen ez az online térben.</h5>
+            <h5>Balogh József vagyok, és azon dolgozom, hogy ez a közösség egyre nagyobbá váljon, és együtt adhassuk
+              tovább ezek az értékeket. Tarts velünk te is!</h5>
+            <h1>De mi az az EDC?</h1>
+            <h5>Egy angol betűszó, amely kibontva az every day carry kifejezést takarja. Ez szó szerinti fordításban
+              azokat a holmikat jelenti, amelyeket nap mint nap magunknál hordunk. A közkeletű tévhittel ellentétben nem
+              szükséges az, hogy mindig minden nap nálunk legyen: inkább egyfajta készletről, gyűjteményről van szó,
+              amelynek elemeit az adott szituációnak megfelelően váltogathatjuk. Más holmikat pakolunk el, ha egy
+              irodába megyünk dolgozni, mást, ha egy építkezésen melózunk, és akkor is, amikor hétvégén rokonlátogatóba
+              megyünk. </h5>
+            <h1>Ha kést szeretnél vásárolni, látogasd meg viszonteladóinkat.</h1>
+
+            <div class="col-sm-10">
+              <div class="d-flex justify-content-center">
+                <a href="index.php?page=resellers" class="big-button btn btn-primary">Viszonteladók</a>
+              </div>
+
+              <div class="d-flex flex-column gap-2 align-items-center m-4">
+                <iframe width="560" src="https://www.youtube.com/watch?v=_Nq5eEQpAFM" title="YouTube videólejátszó"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen></iframe>
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10898.25682619235!2d19.1573916!3d47.4847538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741c5e0ff7f89ad%3A0xf2b4f0f0f205e978!2sBudapest%2C%20Cserkesz%20u.%2032%2C%201105!5e0!3m2!1shu!2shu!4v1718126759721!5m2!1shu!2shu"
+                  width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+              </div>
+
+
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-2"></div> <!-- jobb szél -->
+    </div>
+  </div>
+  <br>
+  <?php include './components/footer.php'; ?>
+
+</body>
+
+</html>
