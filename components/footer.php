ebdd911ed52a49d25e41b2b943b84a1fe7ed8708
radoczsanyi <radocz.sandor1231@example.com>
2025-06-12 20:17:26 +0200

    újrahasznositható lábléc

---
+++ components/footer.php
@@ -0,0 +1,4 @@
+<footer class="text-center py-3 mt-auto bg-dark border-top">
+  <p class="mb-1">Készítő: Kullai Marcell</p>
+  <a href="https://github.com/mk1d" target="_blank" class="text-decoration-none">GitHub</a>
+</footer>
